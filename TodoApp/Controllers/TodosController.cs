c6b1f732244ffcc7711e870463b9fe79bfda632f
samad20 <musab.aljahdhami@gmail.com>
2023-05-04 07:43:53 +0400

    init TodosController with GET method

---
+++ TodoApp/Controllers/TodosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodosController
+    {
+        [HttpGet(Name = "GetTodos")]
+        public string[] Get() {
+            return Array.Empty<string>();
+        }
+    }
+}
