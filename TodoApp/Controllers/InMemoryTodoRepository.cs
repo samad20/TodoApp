59ba799cf1c458540f518cb1f990cdda5c1fed80
samad20 <musab.aljahdhami@gmail.com>
2023-05-09 08:39:43 +0400

    init InMemoryTodoRepository

---
+++ TodoApp/Controllers/InMemoryTodoRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Controllers
+{
+    public class InMemoryTodoRepository : ITodoRepository
+    {
+        public InMemoryTodoRepository() { 
+            Todos = new List<Todo>();
+        }
+        public IEnumerable<Todo> Todos { get; }
+    }
+}
