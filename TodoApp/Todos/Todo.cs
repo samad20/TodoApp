f40fc5ae8bd433a233e86863e41461a648eec505
samad20 <musab.aljahdhami@gmail.com>
2023-05-11 12:56:34 +0400

    adding Id prop

--- TodoApp/Todos/Todo.cs
+++ TodoApp/Todos/Todo.cs
@@ -2,10 +2,12 @@
 {
     public class Todo
     {
+        public int Id { get; private set; }
         public string description { get; private set; }
 
-        public Todo(string description)
+        public Todo(int id, string description)
         {
+            this.Id = id;
             this.description = description;
         }
     }
