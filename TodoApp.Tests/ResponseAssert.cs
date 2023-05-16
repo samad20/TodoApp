6ffcfdf002c1989384f6fdd6f5fdc2f1f301c48e
samad20 <musab.aljahdhami@gmail.com>
2023-05-16 09:15:59 +0400

    adding 400Bad and 201Created response

--- TodoApp.Tests/ResponseAssert.cs
+++ TodoApp.Tests/ResponseAssert.cs
@@ -9,14 +9,24 @@ namespace TodoApp.Tests
 {
     internal static class ResponseAssert
     {
-        public static void Assert200OK(this RestResponse response)
+        public static void Assert200OK(this IRestResponse response)
         {
             Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
         }
-        public static void AssertIs404NotFound(this RestResponse response)
+        public static void AssertIs404NotFound(this IRestResponse response)
         {
             Assert.AreEqual(System.Net.HttpStatusCode.NotFound, response.StatusCode);
         }
-        
+
+        public static void AssertIs400BadRequest(this IRestResponse response)
+        {
+            Assert.AreEqual(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        public static void AssertIs201Created(this IRestResponse response)
+        {
+            Assert.AreEqual(System.Net.HttpStatusCode.Created, response.StatusCode);
+        }
+
     }
 }
