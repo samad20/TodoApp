454d05ee0c3eb1ef291030767582fd56286f9ba0
samad20 <musab.aljahdhami@gmail.com>
2023-05-16 13:19:45 +0400

    adding InMemoryTodoRepository to the service

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -11,8 +11,8 @@ var todos = new List<Todo>()
     new Todo(0,"Say Hello"),
     new Todo(1,"Say Goodbye")
 };
-
-builder.Services.AddScoped<ITodoRepository>((sp)=>new InMemoryTodoRepository(todos));
+var repo = new InMemoryTodoRepository(todos);
+builder.Services.AddScoped<ITodoRepository>((sp) => repo);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
