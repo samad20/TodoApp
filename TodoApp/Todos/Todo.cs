6c50d6efe5c9688096d873304eb979cd09dca392
samad20 <musab.aljahdhami@gmail.com>
2023-05-16 09:17:45 +0400

    adding [MinLength(3)] for description

--- TodoApp/Todos/Todo.cs
+++ TodoApp/Todos/Todo.cs
@@ -1,8 +1,11 @@
-﻿namespace TodoApp.Todos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Todos
 {
     public class Todo
     {
         public int Id { get; private set; }
+        [MinLength(3)]
         public string description { get; private set; }
 
         public Todo(int id, string description)
